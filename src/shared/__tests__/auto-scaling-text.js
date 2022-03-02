29ee3adab9969b4e70c584416748091fb42b55f5
Khanh Luu <luukhanh96@gmail.com>
2022-03-03 00:22:14 +0100

    test

---
+++ src/shared/__tests__/auto-scaling-text.js
@@ -0,0 +1,7 @@
+import {render} from '@testing-library/react'
+import AutoScalingText from '../auto-scaling-text'
+
+test('renders', () => {
+  const {debug} = render(<AutoScalingText />)
+  debug()
+})
